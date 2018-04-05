04f48b6d5a7014939a746c85e6a91238424ab67e
Eetu Mäkelä <eetu.makela@iki.fi>
2018-04-05 14:44:29 +0300

    rename attr to filed in grouping parameters

--- app/parameters/GroupingParameters.scala
+++ app/parameters/GroupingParameters.scala
@@ -8,13 +8,13 @@ import services.IndexAccess
 
 class GroupingParameters(prefix: String = "", suffix: String = "")(implicit request: Request[AnyContent], ia: IndexAccess, queryMetadata: QueryMetadata) {
   private val p = request.body.asFormUrlEncoded.getOrElse(request.queryString)
-  val attrs = p.getOrElse("attr", Seq.empty)
-  val attrLengths = p.getOrElse("attrLength", Seq.empty).map(_.toInt)
-  private val attrTransformerS = p.get("attrTransformer").map(_.head)
+  val attrs = p.getOrElse("field", Seq.empty)
+  val attrLengths = p.getOrElse("fieldLength", Seq.empty).map(_.toInt)
+  private val attrTransformerS = p.get("fieldTransformer").map(_.head)
   val attrTransformer = attrTransformerS.map(apScript => new GroovyShell(RunScriptController.compilerConfiguration).parse(apScript))
   private val grouperS = p.get("grouper").map(_.head)
   val grouper = grouperS.map(apScript => new GroovyShell(RunScriptController.compilerConfiguration).parse(apScript))
-  def toJson = Json.obj(prefix+"attrs"+suffix->attrs,prefix+"attrLengths"+suffix->attrLengths,prefix+"attrTransformer"+suffix->attrTransformerS,prefix+"grouper"+suffix->grouperS)
+  def toJson = Json.obj(prefix+"fields"+suffix->attrs,prefix+"fieldLengths"+suffix->attrLengths,prefix+"fieldTransformer"+suffix->attrTransformerS,prefix+"grouper"+suffix->grouperS)
   def isDefined = !attrs.isEmpty || grouper.isDefined
   queryMetadata.json = queryMetadata.json ++ toJson
 }
