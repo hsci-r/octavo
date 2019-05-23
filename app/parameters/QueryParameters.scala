437864f66c3825ae6f764d23e7b79b26643b2927
Eetu Mäkelä <eetu.makela@iki.fi>
2019-05-23 11:05:39 +0300

    fix parameters

--- app/parameters/QueryParameters.scala
+++ app/parameters/QueryParameters.scala
@@ -6,7 +6,7 @@ import services.IndexAccess
 
 class QueryParameters(prefix: String = "", suffix: String = "")(implicit request: Request[AnyContent], ia: IndexAccess, queryMetadata: QueryMetadata) {
   protected val p = request.body.asFormUrlEncoded.getOrElse(request.queryString)
-  private val defaultLevel: String = p.get(prefix+"level"+suffix).map(_.head).getOrElse(ia.indexMetadata.defaultLevel.id.toUpperCase)
+  private val defaultLevel: String = p.get(prefix+"level"+suffix).orElse(p.get(prefix+"defaultLevel"+suffix)).map(_.head).getOrElse(ia.indexMetadata.defaultLevel.id.toUpperCase)
   val query: Option[String] = p.get(prefix+"query"+suffix).map(v => if (!v.head.startsWith("<")) "<"+defaultLevel+"§"+v.head+"§"+defaultLevel+">" else v.head)
   def requiredQuery: String = query.getOrElse("<"+defaultLevel+"§§"+defaultLevel+">")
   def toJson: JsObject = Json.obj(
